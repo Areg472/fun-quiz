b2ad0efb5bd3a772934a2e6d96c0aa4e4de586e2
Aregus <74340447+Areg472@users.noreply.github.com>
2025-12-26 13:27:19 +0400

    quizzing time!

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
       "Give a answer in 'answer': [answer]" +
       "Give an approval in 'approval': [true/false]" +
       "QUESTION INSTRUCTIONS:" +
-      "Ask a question that most humans know the answer" +
+      "Ask questions as if its a quiz game, and you are the host." +
       "If the user asks 'Give a question to ask the user', don't give the answer" +
       "If the user has answered the question with 50% or more accuracy, ask a harder question and don't give the answer and give a true approval, use false in the answer JSON" +
       "If the user has answered the question with less than 50% accuracy, ask an easier question and give the answer to the question you just asked before the message, and give a false approval" +
